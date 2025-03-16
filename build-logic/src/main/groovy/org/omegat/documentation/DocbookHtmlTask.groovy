55890bef2832373beb921eda8254cec2adf4705d
Hiroshi Miura <miurahr@linux.com>
2025-03-16 09:57:47 +0900

    Refactor parameter handling in DocbookHtmlTask.
    Replaced string-based parameter setting with QName and XdmAtomicValue objects for XsltTransformer. This improves type safety and consistency in parameter definitions, aligning with Saxon's API standards. Simplified and modernized the code for better maintainability.

--- build-logic/src/main/groovy/org/omegat/documentation/DocbookHtmlTask.groovy
+++ build-logic/src/main/groovy/org/omegat/documentation/DocbookHtmlTask.groovy
@@ -1,6 +1,9 @@
 package org.omegat.documentation
 
 import groovy.transform.CompileStatic
+import net.sf.saxon.s9api.QName
+import net.sf.saxon.s9api.XdmAtomicValue
+import net.sf.saxon.s9api.XsltTransformer
 
 @CompileStatic
 class DocbookHtmlTask extends TransformationTask {
@@ -13,27 +16,28 @@ class DocbookHtmlTask extends TransformationTask {
         }
         return fileName.substring(0, extensionIndex)
     }
-/*
+
     @Override
     protected void preTransform(XsltTransformer transformer, File source, File target) {
         String baseDir = outputFile.get().asFile.parent + File.separator
-        transformer.setParameter("root.filename", extractRootName(target))
-        transformer.setParameter("base.dir", baseDir)
-        transformer.setParameter("use.id.as.filename", 1)
-        transformer.setParameter("html.ext", ".html")
-        transformer.setParameter("chunk.section.depth", 0)
-        transformer.setParameter("chunk.first.sections", 0)
-        transformer.setParameter("chunker.output.encoding", "UTF-8")
-        transformer.setParameter("chunker.output.indent", "yes")
-        transformer.setParameter("use.extensions", 1)
-        transformer.setParameter("chapter.autolabel", 0)
-        transformer.setParameter("section.autolabel", 0)
-        transformer.setParameter("tablecolumns.extension", 0)
-        transformer.setParameter("toc.max.depth", 2)
-        transformer.setParameter("generate.toc", "book toc,title,figure,table chapter toc appendix toc")
-        transformer.setParameter("generate.index", 1)
-        transformer.setParameter("html.stylesheet", "omegat.css")
-        transformer.setParameter("docbook.css.link", 0)
-        transformer.setParameter("saxon.character.representation", "native;decimal")
-    }*/
+        transformer.setParameter(new QName("root.filename"), new XdmAtomicValue(extractRootName(target)))
+        transformer.setParameter(new QName("base.dir"), new XdmAtomicValue(baseDir))
+        transformer.setParameter(new QName("use.id.as.filename"), new XdmAtomicValue(1))
+        transformer.setParameter(new QName("html.ext"), new XdmAtomicValue(".html"))
+        transformer.setParameter(new QName("chunk.section.depth"), new XdmAtomicValue(0))
+        transformer.setParameter(new QName("chunk.first.sections"), new XdmAtomicValue(0))
+        transformer.setParameter(new QName("chunker.output.encoding"), new XdmAtomicValue("UTF-8"))
+        transformer.setParameter(new QName("chunker.output.indent"), new XdmAtomicValue("yes"))
+        transformer.setParameter(new QName("use.extensions"), new XdmAtomicValue(1))
+        transformer.setParameter(new QName("chapter.autolabel"), new XdmAtomicValue(0))
+        transformer.setParameter(new QName("section.autolabel"), new XdmAtomicValue(0))
+        transformer.setParameter(new QName("tablecolumns.extension"), new XdmAtomicValue(0))
+        transformer.setParameter(new QName("toc.max.depth"), new XdmAtomicValue(2))
+        transformer.setParameter(new QName("generate.toc"),
+                new XdmAtomicValue("book toc,title,figure,table chapter toc appendix toc"))
+        transformer.setParameter(new QName("generate.index"), new XdmAtomicValue(1))
+        transformer.setParameter(new QName("html.stylesheet"), new XdmAtomicValue("omegat.css"))
+        transformer.setParameter(new QName("docbook.css.link"), new XdmAtomicValue(0))
+        transformer.setParameter(new QName("saxon.character.representation"), new XdmAtomicValue("native;decimal"))
+    }
 }
